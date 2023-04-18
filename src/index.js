ce31251e7a8f0a00393cbd5cf60cada74fcff913
Pushpraj Singh <psingh@imenso.tech>
2023-04-18 12:00:05 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Example from "./example";
+import { DndProvider } from "react-dnd";
+// import Backend from "react-dnd-html5-backend";
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DndProvider backend={HTML5Backend}>
+        <Example />
+      </DndProvider>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
+ 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
